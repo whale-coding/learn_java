3f23882ba5176aeb9f08ddcb9eef0e12a8604795
whale-coding <njq3699@gmail.com>
2023-11-28 16:55:49 +0800

    helloworld

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * @Date 2023-11-25 18:15 星期六
+ * @Author: 聂建强
+ * @Description:
+ */
+public class HelloWorld {
+    /**
+     * Java 程序的入口是 main 方法
+     */
+    public static void main(String[] args) {
+        // 常用输出方式1：System.out.println(输出内容);输出内容，完毕后进行换行
+        System.out.println("HelloWorld");
+        // 常用输出方式2：System.out.print(输出内容);输出内容，完毕后不做任何处理
+        System.out.print("HelloWorld2");
+        System.out.println("HelloWorld3");
+    }
+
+}
